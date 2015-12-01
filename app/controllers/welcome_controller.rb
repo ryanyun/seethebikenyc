546b959db03234262476e0fd2f6bc700136954b6
ryannyunn <ryannyunn@gmail.com>
2015-11-30 22:22:55 -0500

    get coordinates of available citibikes

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,2 +1,8 @@
+require 'open-uri'
 class WelcomeController < ApplicationController
+  def index
+    @coords = []
+    stations = JSON.parse(open("http://www.citibikenyc.com/stations/json").read)['stationBeanList']
+    stations.select{|s| s['statusValue'] == "In Service"}.each{|s| s['availableBikes'].times{@coords << "#{s['latitude']}, #{s['longitude']}"}}
+  end
 end
